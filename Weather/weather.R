f74361e319e922578050f4813fd457618a0be2a4
CountBall <98447556+zeronwide837@users.noreply.github.com>
2023-08-05 16:21:17 +0900

    Update weather.R

--- Weather/weather.R
+++ Weather/weather.R
@@ -21,12 +21,13 @@ abline(h=c(0,1), lty="dotted")
 
 #연속 공변량 MinTemp와 더미형 반응변수 y간 관계를 보는 데 산점도는 한계가 있음
 
-par(mfrow=c(2,1))
 windows(200,100)
+par(mfrow=c(2,1))
 hist(MinTemp[RainTomorrow.1==0],main="No Rain",xlab="MinTemp",xlim=c(-10,30),breaks=seq(-10,30,2.5))
 hist(MinTemp[RainTomorrow.1==1],main="Rain",xlab="MinTemp",xlim=c(-10,30),breaks=seq(-10,30,2.5))
 #비가 온 날에 MinTemp가 큰 경향이 있음(그림1)
 
+par(mfrow=c(1,1))
 MinTemp.c <- cut(MinTemp, quantile(MinTemp, prob=c(0,0.25,0.5,0.75,1),na.rm=T),include.lowest=T) #이산변수로 코딩
 
 mosaicplot(~ MinTemp.c + RainTomorrow, color = c("white","gray"),xlab = "MinTemp", main=paste("weather",3)) #Q4에서 비온날의 비율이 높음
@@ -34,6 +35,7 @@ mosaicplot(~ MinTemp.c + RainTomorrow, color = c("white","gray"),xlab = "MinTemp
 cut.x <- function(var) cut(var,unique(quantile(var,prob=c(0,0.25,0.5,0.75,1),na.rm=T)),include.lowest = T)
 
 detach(weather)
+weather2 <- data.frame(weather)
 attach(weather2)
 
 for (j in c(3:22)){
